3ce93fd7e5701ad05f00675267d6dadc623b866f
karolinasenkow <karolinasenkow@gmail.com>
2020-04-11 03:38:31 -0500

    Added Sort class.

--- src/Phonebook/Main.java
+++ src/Phonebook/Main.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
+import static Phonebook.Sort.findName;
+
 public class Main {
 
 
@@ -14,6 +16,7 @@ public class Main {
     final Map<String, String> phoneBook = new TreeMap<>();
     BufferedReader reader;
     int size = phoneBook.size();
+    int choice;
     Scanner input = new Scanner(System.in);
     File userHomeDirectory = new File(System.getProperty("user.dir"));
 
@@ -45,11 +48,32 @@ public class Main {
         }
       }
     }
-
+    else {
+      System.out.println("Invalid input.");
+      throw new IllegalArgumentException();
+    }
     ArrayList<Map.Entry<String, String>> arr = new ArrayList<>(size);
     arr.addAll(phoneBook.entrySet());
     for (Map.Entry<String, String> mapEntry : arr) {
       System.out.println(mapEntry);
+    }
+
+    System.out.println("What would you like to do today? Type the number of the action you would like to perform.");
+    System.out.println("1. Search for a name.");
+    System.out.println("2. Search for a phone number.");
+    Scanner in = new Scanner(System.in);
+    if (in.hasNextInt()) {
+      choice = in.nextInt();
+    } else {
+      System.out.println("Please enter a valid choice.");
+      throw new IllegalArgumentException();
+    }
+    switch (choice) {
+      case 1:
+        System.out.println("Enter name.");
+        String name = in.nextLine();
+        System.out.println(findName(arr, name));
+
     }
   }
 }
